09127e537f59e66a480e56276ebb477ec99bc4b6
gustavo <gustavo98ag@gmail.com>
2025-01-03 15:28:48 -0300

    Início do projeto

---
+++ src/main/java/br/com/gamagustavo/carteirafinanceira/CarteiraFinanceiraApplication.java
@@ -0,0 +1,13 @@
+package br.com.gamagustavo.carteirafinanceira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteiraFinanceiraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarteiraFinanceiraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/gamagustavo/carteirafinanceira/CarteiraFinanceiraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gamagustavo.carteirafinanceira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiraFinanceiraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
